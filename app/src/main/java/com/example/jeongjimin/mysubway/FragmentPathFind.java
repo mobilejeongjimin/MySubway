65159207f65cfef4801f3ea4f316b11f9b736792
jeongjimin <myong0202@gmail.com>
2018-05-22 15:45:48 +0900

    PathfindActivity > PathfindComputeActivity > FragmentPathfind 순으로 데이터 넘기는 것 테스트구현

--- app/src/main/java/com/example/jeongjimin/mysubway/FragmentPathFind.java
+++ app/src/main/java/com/example/jeongjimin/mysubway/FragmentPathFind.java
@@ -5,13 +5,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class FragmentPathFind extends Fragment{
+
+    private TextView startStation;
+    private TextView destiStation;
+    private TextView timeTxt;
+    private TextView station;
+    private TextView transferStation;
+
+    private String startStationtxt;
+    private String destiStationtxt;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pathfind_view, container, false);
 
+        startStation = view.findViewById(R.id.startstationtxt);
+        destiStation = view.findViewById(R.id.destistationtxt);
+
+        Bundle bundle = getArguments();
+
+        if(bundle != null) {
+            startStationtxt = bundle.getString("FragStartStation");
+            destiStationtxt = bundle.getString("FragDestiStation");
+
+            startStation.setText(startStationtxt);
+            destiStation.setText(destiStationtxt);
+        }
+
         return view;
     }
+
 }
