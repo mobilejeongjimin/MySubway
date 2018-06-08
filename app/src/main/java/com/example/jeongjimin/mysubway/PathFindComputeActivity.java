1409e607d35210025c6ff37d6f5233337e38b6fb
choiyunseok1 <choiys1995@gmail.com>
2018-06-08 10:25:09 +0900

    api에서 데이터 파싱해서 가져오기. 화면에 띄우기 진행중

--- app/src/main/java/com/example/jeongjimin/mysubway/PathFindComputeActivity.java
+++ app/src/main/java/com/example/jeongjimin/mysubway/PathFindComputeActivity.java
@@ -35,6 +35,11 @@ public class PathFindComputeActivity extends AppCompatActivity {
     private String data;
     private String Key = "704c42506963686f3231696c676469";
 
+    private String TakeTime;
+    private String StationnNm;
+    private String TrensferStationnNm;
+    private String StationName;
+
     private Button FragmentPathFindButton;
     private Button FragmentMapButton;
 
@@ -74,6 +79,158 @@ public class PathFindComputeActivity extends AppCompatActivity {
         }).start();
 
         SelectFragment(FragmentPathFindButton);
+
+        StrictMode.enableDefaults();
+
+        boolean instatnFid = false, instatnTid = false, instatnFnm = false, instatnTnm = false, inshtStatnId = false, inrow = false;
+        boolean inshtStatnNm = false, inshtTransferMsg = false, inshtTravelMsg = false, inshtStatnCnt = false, inshtTravelTm = false, inshtTransferCnt = false;
+
+        String statnFid = null, statnTid = null, statnFnm = null, statnTnm = null, shtStatnId = null, shtStatnNm = null, shtTransferMsg =null, shtTravelMsg =null;
+        String shtStatnCnt = null, shtTravelTm = null, shtTransferCnt = null;
+
+        try{
+
+            URL url = new URL("http://swopenapi.seoul.go.kr/api/subway/72574f735463686f36344f666b6645" +
+                    "/xml/shortestRoute/0/1/"+StartStation+"/"+DestiStation); //검색 URL부분
+
+            XmlPullParserFactory parserCreator = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = parserCreator.newPullParser();
+
+            parser.setInput(url.openStream(), null);
+
+            int parserEvent = parser.getEventType();
+            System.out.println("파싱시작합니다.");
+
+            while (parserEvent != XmlPullParser.END_DOCUMENT){
+
+                switch(parserEvent){
+                    case XmlPullParser.START_TAG://parser가 시작 태그를 만나면 실행
+//                        if(parser.getName().equals("statnFid")){ //출발역 아이디
+//                            instatnFid = true;
+//                        }
+
+//                        if(parser.getName().equals("statnTid")){ //도착역 아이디
+//                            instatnTid = true;
+//                        }
+
+                        if(parser.getName().equals("statnFnm")){ //출발역 이름
+                            instatnFnm = true;
+                        }
+
+                        if(parser.getName().equals("statnTnm")){ //도착역 이름
+                            instatnTnm = true;
+                        }
+
+//                        if(parser.getName().equals("shtStatnId")){ //최단시간 경유 전체 아이디
+//                            inshtStatnId = true;
+//                        }
+
+                        if(parser.getName().equals("shtStatnNm")){ //최단시간 경유 전체 이름
+                            inshtStatnNm = true;
+                        }
+
+//                        if(parser.getName().equals("shtTransferMsg")){ //환승 지하철역 몇분 걸리고 몇개 있습니다
+//                            inshtTransferMsg = true;
+//                        }
+
+//                        if(parser.getName().equals("shtTravelMsg")){ //최단시간 지하철역 몇분 걸리고 몇개 있습니다.
+//                            inshtTravelMsg = true;
+//                        }
+
+                        if(parser.getName().equals("shtStatnCnt")){ //경유 지하철 역 수
+                            inshtStatnCnt = true;
+                        }
+
+                        if(parser.getName().equals("shtTravelTm")){ //도착 예정시간(분단위)
+                            inshtTravelTm = true;
+                        }
+
+                        if(parser.getName().equals("shtTransferCnt")){ //환승 횟수
+                            inshtTransferCnt = true;
+                        }
+//
+//                        if(parser.getName().equals("message")){ //message 태그를 만나면 에러 출력
+//                            status1.setText(status1.getText()+"에러");
+//                            //여기에 에러코드에 따라 다른 메세지를 출력하도록 할 수 있다.
+//
+//                        }
+                        break;
+
+                    case XmlPullParser.TEXT://parser가 내용에 접근했을때
+
+//                        if(instatnFid){ //isTitle이 true일 때 태그의 내용을 저장.
+//                            statnFid = parser.getText();
+//                            instatnFid = false;
+//                        }
+
+//                        if(instatnTid){ //isAddress이 true일 때 태그의 내용을 저장.
+//                            statnTid = parser.getText();
+//                            instatnTid = false;
+//                        }
+
+                        if(instatnFnm){ //isMapx이 true일 때 태그의 내용을 저장.
+                            statnFnm = parser.getText();
+                            instatnFnm = false;
+                        }
+
+                        if(instatnTnm){ //isMapy이 true일 때 태그의 내용을 저장.
+                            statnTnm = parser.getText();
+                            instatnTnm = false;
+                        }
+
+//                        if(inshtStatnId){ //isMapy이 true일 때 태그의 내용을 저장.
+//                            shtStatnId = parser.getText();
+//                            inshtStatnId = false;
+//                        }
+
+                        if(inshtStatnNm){ //isMapy이 true일 때 태그의 내용을 저장.
+                            shtStatnNm = parser.getText();
+                            inshtStatnNm = false;
+                        }
+
+//                        if(inshtTransferMsg){ //isMapy이 true일 때 태그의 내용을 저장.
+//                            shtTransferMsg = parser.getText();
+//                            inshtTransferMsg = false;
+//                        }
+
+//                        if(inshtTravelMsg){ //isMapy이 true일 때 태그의 내용을 저장.
+//                            shtTravelMsg = parser.getText();
+//                            inshtTravelMsg = false;
+//                        }
+
+                        if(inshtStatnCnt){ //isMapy이 true일 때 태그의 내용을 저장.
+                            shtStatnCnt = parser.getText();
+                            inshtStatnCnt = false;
+                        }
+
+                        if(inshtTravelTm){ //isMapy이 true일 때 태그의 내용을 저장.
+                            shtTravelTm = parser.getText();
+                            inshtTravelTm = false;
+                        }
+
+                        if(inshtTransferCnt){ //isMapy이 true일 때 태그의 내용을 저장.
+                            shtTransferCnt = parser.getText();
+                            inshtTransferCnt = false;
+                        }
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        if(parser.getName().equals("row")){
+//                            status1.setText(status1.getText()+"출발지하철역ID : "+ statnFid +"\n 도착지하철역ID: "+ statnTid +"\n 출발지하철역명 : " + statnFnm+
+//                                    "\n 도착지하철역명 : " + statnTnm +  "\n 최단시간 경유 지하철역ID : " + shtStatnId+ "\n 최단시간 경유 지하철역명 : " + shtStatnNm
+//                                    +"\n 최단시간 환승 지하철역명: " +shtTransferMsg + "\n 최단시간 도착 예정메세지 : " + shtTravelMsg + "\n 최단시간 경유 지하철역수 : " +shtStatnCnt
+//                                    +"\n 최단시간 도착 예정시간 : " +shtTravelTm +"\n 최단시간 환승횟수 : " +shtTransferCnt+"\n");
+//
+                            inrow = false;
+                        }
+                        break;
+                }
+                parserEvent = parser.next();
+            }
+        } catch(Exception e){
+//            status1.setText("에러가..났습니다...");
+        }
+
 
 
     }
@@ -253,5 +410,3 @@ public class PathFindComputeActivity extends AppCompatActivity {
 
 
 }
-
-
